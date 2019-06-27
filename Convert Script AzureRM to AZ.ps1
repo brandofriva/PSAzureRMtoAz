d60790fd635002c841b676f2c5d7d15e447b9a21
brandofriva <nicholas.brand@gmail.com>
2019-06-27 08:43:46 +0200

    Add files via upload

---
+++ Convert Script AzureRM to AZ.ps1
@@ -0,0 +1,16 @@
+﻿#Original Script: https://bartsimons.me/convert-azurerm-scripts-to-az/
+
+$Mappings = ((Invoke-WebRequest https://raw.githubusercontent.com/Azure/azure-powershell/master/src/Accounts/Accounts/AzureRmAlias/Mappings.json -UseBasicParsing).Content | ConvertFrom-Json)
+
+$ScriptFile = "C:\scripts\script.ps1"
+$Script = (Get-Content $ScriptFile -Raw)
+
+($Mappings | Get-Member -MemberType NoteProperty) | % {
+    $Mappings.$($_.Name) | % {
+        ForEach ($Mapping in ($_ | Get-Member -MemberType NoteProperty)) {
+            $Script = $Script -replace $_.$($Mapping.Name),$Mapping.Name
+        }
+    }
+}
+
+$Script | Set-Content $ScriptFile
